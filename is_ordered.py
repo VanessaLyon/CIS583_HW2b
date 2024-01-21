7dc803afedfae71567d6c6cbf40f724b497ec8a2
VanessaLyon <62832343+VanessaLyon@users.noreply.github.com>
2024-01-20 19:09:03 -0500

    Update is_ordered.py
    daphne is back 2

--- is_ordered.py
+++ is_ordered.py
@@ -23,53 +23,43 @@ else:
 	Conveniently, most type 2 transactions set the gasPrice field to be min( tx.maxPriorityFeePerGas + block.baseFeePerGas, tx.maxFeePerGas )
 """
 
-from web3 import Web3
-
-rpc_url = "https://eth-mainnet.alchemyapi.io/v2/7R8FD0Z9VuycQYgASfO5xsfAPsK21DJW"
-w3 = Web3(Web3.HTTPProvider(rpc_url))
-
-if w3.is_connected():
-    pass
-else:
-    print("Failed to connect to Ethereum node!")
-
-
 def is_ordered_block(block_num):
     block = w3.eth.get_block(block_num)
     ordered = False
 
-        if block_num <= 12965000:  # Pre-London Hard Fork
+    if block_num <= 12965000:  # Pre-London Hard Fork
         ordered = all(
-                (
+            (
                 tx["gas_price"] if isinstance(tx, dict) else dict(tx)["gas_price"]
-                ) >= (
+            ) >= (
                 block.transactions[i + 1]["gas_price"]
                 if isinstance(block.transactions[i + 1], dict)
                 else dict(block.transactions[i + 1])["gas_price"]
-                )
-                for i, tx in enumerate(block.transactions[:-1])
+            )
+            for i, tx in enumerate(block.transactions[:-1])
         )
-        else:  # Post-London Hard Fork
+    else:  # Post-London Hard Fork
         ordered = all(
+            (
                 (
-                (
-                        tx.get("max_priority_fee_per_gas", 0) + block.base_fee_per_gas
+                    tx.get("max_priority_fee_per_gas", 0) + block.base_fee_per_gas
                 )
                 if isinstance(tx, dict)
                 else dict(tx).get("max_priority_fee_per_gas", 0) + block.base_fee_per_gas
-                ) >= (
+            ) >= (
                 (
-                        block.transactions[i + 1].get("max_priority_fee_per_gas", 0) +
-                        block.base_fee_per_gas
+                    block.transactions[i + 1].get("max_priority_fee_per_gas", 0) +
+                    block.base_fee_per_gas
                 )
                 if isinstance(block.transactions[i + 1], dict)
                 else dict(block.transactions[i + 1]).get("max_priority_fee_per_gas", 0) +
                 block.base_fee_per_gas
-                )
-                for i, tx in enumerate(block.transactions[:-1])
+            )
+            for i, tx in enumerate(block.transactions[:-1])
         )
 
     return ordered
+
 
 
 """
